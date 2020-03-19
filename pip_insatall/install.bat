61bfde81f08e700adb627c1a475b4fc861360e8b
SongZihuan <jimsong2010@163.com>
2020-03-19 17:31:24 +0800

    add install.bak and install.sh

---
+++ pip_insatall/install.bat
@@ -0,0 +1,17 @@
+pip install Pillow==7.0.0
+pip install pygame==1.9.6
+pip install selenium==3.141.0
+pip install requests==2.22.0
+pip install bs4==0.0.1
+pip install urllib3==1.24.3
+pip install Gitpython==3.1.0
+pip install numpy==1.18.1
+pip install pandas==0.25.3
+pip install pandas-profiling==2.5.0
+pip install scipy==1.4.1
+pip install sympy==1.5.1
+pip install matplotlib
+pip install pyecharts==1.7.0
+pip install sklearn
+pip install pip==20.0.2
+
